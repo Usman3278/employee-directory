e93194827b4bc45f40d0b1e544045156bb0d4f49
Usman3278 <usman3278@gmail.com>
2021-04-01 12:19:49 -0400

    added gh-pages

---
+++ build/precache-manifest.72939a5bac15ab18f6927c7945fdcb52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcc13b67e993ac859dc9fdb976f8d68b",
+    "url": "/Usman3278/employee_directory/index.html"
+  },
+  {
+    "revision": "8d5df93187952a13d86b",
+    "url": "/Usman3278/employee_directory/static/css/main.ac93ded3.chunk.css"
+  },
+  {
+    "revision": "377a4bd9e4f2158e4c01",
+    "url": "/Usman3278/employee_directory/static/js/2.f853ab81.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Usman3278/employee_directory/static/js/2.f853ab81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d5df93187952a13d86b",
+    "url": "/Usman3278/employee_directory/static/js/main.512a2919.chunk.js"
+  },
+  {
+    "revision": "17446db2eb0dd99e97e1",
+    "url": "/Usman3278/employee_directory/static/js/runtime-main.9fbefada.js"
+  }
+]);
